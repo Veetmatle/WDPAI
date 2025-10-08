25c6297269d8e4a95bb82300fa9836eb18daf310
Andrzej Laskowski <xaveer17@gmail.com>
2025-10-08 15:38:39 +0200

    Nowe pliki, nowy commit

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
 
-echo "<h1>Hello students!<h1>";
+require 'Routing.php';
+
+// kod 418 - czas na kawe
+// MVC - monolit (widoki, css, js, php osobno itd.)
+// inna architektura - frontend i backend (php, sql)
+// index.php - bootstrap (front controller)
+
+$path = trim($_SERVER['REQUEST_URI'], '/'); // wywala białe znaki
+$path = parse_url($path, PHP_URL_PATH); // pobiera drugą część linka
+
+// var_dump($path); // taki print do zmiennych
+
+Routing::run($path);
+?>
