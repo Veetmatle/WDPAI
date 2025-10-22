a10f00a701ef00e19a4b94248e6c960d6eb86737
Andrzej Laskowski <xaveer17@gmail.com>
2025-10-22 15:58:36 +0200

    Css + html + routing zajecia

--- Routing.php
+++ Routing.php
@@ -1,20 +1,33 @@
 <?php
 
+require_once 'src/controllers/SecurityController.php';
+require_once 'src/controllers/DashboardController.php';
+class Routing 
+{
+    public static $routes = [
+        'login' => ['controller' => 'SecurityController', 'action' => 'login'],
+        'dashboard' => ['controller' => 'DashboardController', 'action' => 'index'],
+    ];
 
-class Routing {
-
-    public static function run(string $path) {
+    public static function run(string $path) 
+    {
         //TODO na podstawie sciezki sprawdzamy jaki HTML zwrocic
-        switch ($path) {
-    case 'dashboard':
-        include 'public/views/dashboard.html';
-        break;
-    case 'login':
-        include 'public/views/login.html';
-        break;
-    default:
-        include 'public/views/404.html';
-        break;
-} 
+        switch ($path) 
+        {
+        case 'dashboard':
+            // co jeśli user przekaze zmienna, jak przekazac zmienna do akcji z kontrolera
+            $controller = new Routing::$routes['dashboard']['controller'];
+            $action = Routing::$routes['dashboard']['action'];
+            $controller->$action();
+            break;
+        case 'login':
+            $controller = new Routing::$routes['login']['controller'];
+            $action = Routing::$routes['login']['action'];
+            $controller->$action();
+            break;
+        default:
+            include 'public/views/404.html';
+            break;
+        } 
     }
 }
